70bca884f7161f15abda987bca2b504b8038f029
Valerie Concepcion <valerie.concepcion@nttdata.com>
2014-02-05 12:21:21 -0600

    Fix invalid config setting and add custom stylesheet for Bucket Search UI

--- Source/Website/Scripts/heisenberg.js
+++
@@ -1,6 +0,0 @@
-﻿(function ($) {
-    $(document).ready(function () {
-        $('#WorkflowView').style('background', 'url("/sitecore/shell/Applications/Buckets/images/heisenberg.png")');
-        $('#WorkflowView:hover, #WorkflowView.active').style('background', 'url("/sitecore/shell/Applications/Buckets/images/heisenberg_selected.png")');
-    });
-})(jQuery);
